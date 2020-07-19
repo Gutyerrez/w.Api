2a7b8d64925cf5c722270271a6e4666a2f4de6da
Gutyerrez <srgutyerrez@gmail.com>
2020-07-19 06:36:22 -0300

    dev

--- server/app/Controllers/Http/AuthController.ts
+++ server/app/Controllers/Http/AuthController.ts
@@ -1,11 +1,23 @@
 import Env from '@ioc:Adonis/Core/Env'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { InvalidApplicationException } from 'App/Exceptions/InvalidApplicationException'
 
 import jwt from 'jsonwebtoken'
 
 export default class AuthController {
 
   public async store({ request }: HttpContextContract) {
+    const apps = JSON.parse(Env.get('APPS') as string) as string[]
+
+    const app = request.headers()['application'] as string
+
+    if (!apps.find(e => e === app)) {
+      throw new InvalidApplicationException(
+        'Can\'t find informed application',
+        'E_UNKNOWN_APPLICATION'
+      )
+    }
+
     const route = request.headers()['route'] || '/'
 
     const access_token = jwt.sign(
