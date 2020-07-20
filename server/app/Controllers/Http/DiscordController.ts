bdd64fdfe970ccd6aed825b2a5b9fb6044e2010f
Gutyerrez <srgutyerrez@gmail.com>
2020-07-20 06:55:41 -0300

    dev

--- server/app/Controllers/Http/DiscordController.ts
+++ server/app/Controllers/Http/DiscordController.ts
@@ -1,4 +1,65 @@
-// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Database from '@ioc:Adonis/Lucid/Database'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { Discord } from '../../../misc/discord'
+
+import jwt from 'jsonwebtoken'
 
 export default class DiscordController {
+
+  public async store({ request }: HttpContextContract) {
+    const code = request.input('code')
+    const user_id = request.input('user_id')
+    const token = request.input('token')
+
+    const payload = jwt.decode(token)
+
+    if (!payload) {
+      return {
+        code: 400,
+        message: 'Invalid payload token'
+      }
+    }
+
+    const payload_user_id = payload['user_id']
+
+    if (payload_user_id !== user_id) {
+      return {
+        code: 401,
+        message: 'Invalid payload token'
+      }
+    }
+
+    const discordUser = await Discord.fetch(code)
+
+    if (!discordUser) {
+      return {
+        code: 500,
+        message: 'Internal server error'
+      }
+    }
+
+    const exists = await Database.from('users')
+      .select('*')
+      .where('discord_id', discordUser.id)
+      .first()
+
+    if (exists) {
+      return {
+        code: 401,
+        message: 'Your disocrd account is already synchronized'
+      }
+    }
+
+    const updated = await Database.from('users')
+      .select('*')
+      .where('user_id', user_id)
+      .update({
+        discord_id: discordUser.id
+      })
+
+    return {
+      updated
+    }
+  }
+
 }
---
+++ server/misc/discord/index.ts
@@ -0,0 +1,68 @@
+import Env from '@ioc:Adonis/Core/Env'
+
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: 'https://discord.com/api/v6'
+})
+
+declare interface AccessTokenResponse {
+  access_token: string,
+  token_type: string,
+  expires_in: Number,
+  refresh_token: string,
+  scop: string
+}
+
+declare interface DiscordUser {
+  id: string,
+  username: string,
+  discriminator: string,
+  avatar: string,
+  bot?: boolean,
+  system?: boolean,
+  mfa_enabled?: boolean,
+  locale?: string,
+  verified?: boolean,
+  email?: string,
+  flags?: Number,
+  premium_type?: Number,
+  public_flags?: Number
+}
+
+export class Discord {
+  protected static async exchangeCode(code: string) {
+    const response = await axios.post<AccessTokenResponse>('/oauth2/token', {
+      client_id: Env.get('CLIENT_ID') as string,
+      client_secret: Env.get('CLIENT_SECRET') as string,
+      grant_type: 'authorization_code',
+      code,
+      redirect_url: Env.get('REDIRECT_URI'),
+      scope: 'identify email connections'
+    }, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
+
+    return response.data
+  }
+
+  protected static async authorize({ token_type, access_token }: AccessTokenResponse) {
+    const response = await api.get<DiscordUser>('/users/@me', {
+      headers: {
+        'Authorization': `${token_type} ${access_token}`
+      }
+    })
+
+    return response.data
+  }
+
+  static async fetch(code: string) {
+    const exchanged = await Discord.exchangeCode(code)
+
+    return await Discord.authorize(exchanged)
+  }
+}
+
+export default api
